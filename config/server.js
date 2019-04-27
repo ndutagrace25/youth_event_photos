c4cb414cd0458794730d8b17caa99eb7e7a11e42
ndutagrace25 <ndutagrace25@gmail.com>
2019-04-27 16:41:37 +0300

    first commit

---
+++ config/server.js
@@ -0,0 +1,5 @@
+module.exports = {
+    APPLICATION_ID: process.env.APPLICATION_ID || '638492567c78ec921b120a61f983d93b03373c0b9c4ed2e9d1560f5ccde88fe3',
+    SECRET: process.env.SECRET || '0bf36678ab63b869301d9865eca4014972d02e6159b162b6168a6a4ab8d9d9ce',
+    CALLBACK_URL: process.env.CALLBACK_URL || 'http://localhost:3000'
+};
